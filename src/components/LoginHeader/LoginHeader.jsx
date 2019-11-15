150a2958470038aeb9a736e3bc5cdd53ab6c139d
charles <codelyfewithcharlie@gmail.com>
2019-11-15 03:35:55 -0700

    updates

--- src/components/LoginHeader/LoginHeader.jsx
+++ src/components/LoginHeader/LoginHeader.jsx
@@ -6,52 +6,51 @@ import babolat from "../../assets/babolat-logo-min.png";
 import wilson from "../../assets/wilson-logo-min.png";
 import adidas from "../../assets/Adidas-Logo-min.png";
 import prince from "../../assets/prince-logo-min.png";
-import { logout } from "../../redux/actions";
+import { logout, emptyCart } from "../../redux/actions";
 import { connect } from "react-redux";
 class LoginHeader extends React.Component {
-	logout = () => {
-		axios.delete("/api/logout").then(res => {
-			this.props.logout();
-		});
-	};
+  logout = () => {
+    axios.delete("/api/logout").then(res => {
+      this.props.emptyCart();
+      this.props.logout();
+    });
+  };
 
-	render() {
-		return (
-			<nav className={style.LoginNav}>
-				<div className={style.LogoBox}>
-					<img src={babolat} alt='babolat' />
-					<img src={wilson} alt='wilson' />
-					<img src={adidas} alt='adidas' />
-					<img src={prince} alt='prince' />
-				</div>
+  render() {
+    return (
+      <nav className={style.LoginNav}>
+        <div className={style.LogoBox}>
+          <img src={babolat} alt="babolat" />
+          <img src={wilson} alt="wilson" />
+          <img src={adidas} alt="adidas" />
+          <img src={prince} alt="prince" />
+        </div>
 
-				{/* this ternary switches between signin/register & logout links based on if there is a user */}
-				{!this.props.currentUser ? (
-					<div className={style.LoginBox}>
-						<Link to='/stats'>STATS & MATCHES</Link>
-						<Link to='/signin'>Sign In</Link>
-						<Link to='/register'>Register</Link>
-					</div>
-				) : (
-					<div className={style.LoginBox}>
-						<Link to='/stats'>STATS & MATCHES</Link>
-						<button onClick={() => this.logout()}>Logout</button>
-					</div>
-				)}
-			</nav>
-		);
-	}
+        {/* this ternary switches between signin/register & logout links based on if there is a user */}
+        {!this.props.currentUser ? (
+          <div className={style.LoginBox}>
+            <Link to="/stats">STATS & MATCHES</Link>
+            <Link to="/signin">Sign In</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        ) : (
+          <div className={style.LoginBox}>
+            <Link to="/stats">STATS & MATCHES</Link>
+            <button onClick={() => this.logout()}>Logout</button>
+          </div>
+        )}
+      </nav>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return state.user;
+  return state.user;
 };
 
 const mapDispatchToProps = {
-	logout
+  logout,
+  emptyCart
 };
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(LoginHeader);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginHeader);
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import "./Profile.scss";
 import userPhoto from "../../assets/userPhoto.png";
 import { updateEmail, logout, login } from "../../redux/actions";
@@ -8,119 +9,125 @@ import BasicHeader from "../../components/BasicHeader/BasicHeader";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 class Profile extends Component {
-	state = {
-		currentPass: "",
-		newPass: "",
-		newEmail: "",
-		passSet: false
-	};
+  state = {
+    currentPass: "",
+    newPass: "",
+    newEmail: "",
+    passSet: false
+  };
 
-	updateEmail = async (newEmail, user_id) => {
-		const result = await axios.put("/api/updateEmail", {
-			newEmail,
-			user_id
-		});
-		const updatedEmail = result.data[0].email;
-		this.props.updateEmail(updatedEmail);
-		this.props.logout();
-		this.props.history.push("/signin");
-	};
+  updateEmail = async (newEmail, user_id) => {
+    const result = await axios.put("/api/updateEmail", {
+      newEmail,
+      user_id
+    });
+    const updatedEmail = result.data[0].email;
+    this.props.updateEmail(updatedEmail);
+    this.props.logout();
+    this.props.history.push("/signin");
+  };
 
-	updatePass = async (currentPass, newPass, user) => {
-		await axios.put("/api/updatePass", {
-			currentPass,
-			newPass,
-			user
-		});
-		await this.props.logout();
-		this.props.history.push("/signin");
-	};
+  updatePass = async (currentPass, newPass, user) => {
+    await axios.put("/api/updatePass", {
+      currentPass,
+      newPass,
+      user
+    });
+    await this.props.logout();
+    this.props.history.push("/signin");
+  };
 
-	render() {
-		return (
-			<div>
-				{/* {!this.props.user.currentUser ? (
-					<div>
-						<h3>Login</h3>
-						<Link to='/signin'>GO TO LOGIN</Link>
-					</div>
-				) : ( */}
-				<div>
-					<BasicHeader />
-					<Header />
-					<section className='profile-section'>
-						<div className='form-section'>
-							<img src={userPhoto} alt='user-default' />
-							<h2>Current Email: Current Email</h2>
-							<form
-								onSubmit={e => {
-									e.preventDefault();
-									this.updateEmail(
-										this.state.newEmail,
-										this.props.user.currentUser.id
-									);
-								}}
-							>
-								<input
-									value={this.state.newEmail}
-									type='email'
-									onChange={e => {
-										this.setState({ newEmail: e.target.value });
-									}}
-									placeholder='Enter New Email'
-								/>
-								<button>Update email</button>
-							</form>
-							<form
-								className='password-form'
-								onSubmit={e => {
-									e.preventDefault();
-									this.updatePass(
-										this.state.currentPass,
-										this.state.newPass,
-										this.props.user.currentUser.email
-									);
-								}}
-							>
-								<input
-									value={this.state.currentPass}
-									type='password'
-									onChange={e => {
-										this.setState({ currentPass: e.target.value });
-									}}
-									placeholder='Enter Current Password'
-								/>
-								<input
-									value={this.state.newPass}
-									onChange={e => {
-										this.setState({ newPass: e.target.value });
-									}}
-									type='password'
-									placeholder='Enter New Password'
-								/>
-								<button>Update Password</button>
-							</form>
-						</div>
-					</section>
-				</div>
-				{/* )} */}
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {!this.props.user.currentUser ? (
+          <div>
+            <h3>Login</h3>
+            <Link to="/signin">GO TO LOGIN</Link>
+          </div>
+        ) : (
+          <div>
+            {this.props.user.currentUser.id !==
+            Number(this.props.match.params.id) ? (
+              <div>
+                <h2>This is not your profile</h2>
+              </div>
+            ) : (
+              <div>
+                <BasicHeader />
+                <Header />
+                <section className="profile-section">
+                  <div className="form-section">
+                    <img src={userPhoto} alt="user-default" />
+                    <h2>Current Email: Current Email</h2>
+                    <form
+                      onSubmit={e => {
+                        e.preventDefault();
+                        this.updateEmail(
+                          this.state.newEmail,
+                          this.props.user.currentUser.id
+                        );
+                      }}
+                    >
+                      <input
+                        value={this.state.newEmail}
+                        type="email"
+                        onChange={e => {
+                          this.setState({ newEmail: e.target.value });
+                        }}
+                        placeholder="Enter New Email"
+                      />
+                      <button>Update email</button>
+                    </form>
+                    <form
+                      className="password-form"
+                      onSubmit={e => {
+                        e.preventDefault();
+                        this.updatePass(
+                          this.state.currentPass,
+                          this.state.newPass,
+                          this.props.user.currentUser.email
+                        );
+                      }}
+                    >
+                      <input
+                        value={this.state.currentPass}
+                        type="password"
+                        onChange={e => {
+                          this.setState({ currentPass: e.target.value });
+                        }}
+                        placeholder="Enter Current Password"
+                      />
+                      <input
+                        value={this.state.newPass}
+                        onChange={e => {
+                          this.setState({ newPass: e.target.value });
+                        }}
+                        type="password"
+                        placeholder="Enter New Password"
+                      />
+                      <button>Update Password</button>
+                    </form>
+                  </div>
+                </section>
+              </div>
+            )}
+          </div>
+        )}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return state;
+  return state;
 };
 
 const mapDispatchToProps = {
-	updateEmail,
-	logout,
-	login
+  updateEmail,
+  logout,
+  login
 };
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Profile);
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
