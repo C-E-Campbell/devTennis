329865894da7013b963b6d4bf02be67445e8eccc
charles <codelyfewithcharlie@gmail.com>
2019-11-15 03:38:19 -0700

    updates

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -2,43 +2,43 @@ import React from "react";
 import "./Email.scss";
 import axios from "axios";
 class Email extends React.Component {
-	state = {
-		email: ""
-	};
-	sendDiscount = email => {
-		axios.post("/api/discount", {
-			sendTo: this.state.email
-		});
-		this.setState({ email: "" });
-	};
-	render() {
-		return (
-			<div className='email'>
-				<div className='email-container'>
-					<p className='email-text'>
-						Sign up and receive <span>15% OFF</span> coupon!
-					</p>
-					<form
-						onSubmit={async e => {
-							e.preventDefault();
-							await this.sendDiscount(this.state.email);
-						}}
-						className='form-box'
-					>
-						<input
-							value={this.state.email}
-							onChange={e => {
-								this.setState({ email: e.target.value });
-							}}
-							type='email'
-							placeholder='Enter Email'
-						/>
-						<button type='submit'>Submit</button>
-					</form>
-				</div>
-			</div>
-		);
-	}
+  state = {
+    email: ""
+  };
+  sendDiscount = email => {
+    axios.post("/api/discount", {
+      sendTo: this.state.email
+    });
+    this.setState({ email: "" });
+  };
+  render() {
+    return (
+      <div className="email">
+        <div className="email-container">
+          <p className="email-text">
+            Sign up and receive <span>15% OFF</span> !
+          </p>
+          <form
+            onSubmit={async e => {
+              e.preventDefault();
+              await this.sendDiscount(this.state.email);
+            }}
+            className="form-box"
+          >
+            <input
+              value={this.state.email}
+              onChange={e => {
+                this.setState({ email: e.target.value });
+              }}
+              type="email"
+              placeholder="Enter Email"
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Email;
