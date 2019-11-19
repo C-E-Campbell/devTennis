b1a3ef01f18a338beda068be7b0848f9f8292157
charles <codelyfewithcharlie@gmail.com>
2019-11-19 15:40:23 -0700

    updates

--- src/components/ScrollOver/ScrollOver.jsx
+++ src/components/ScrollOver/ScrollOver.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import style from "./ScrollOver.module.scss";
 export default function ScrollOver() {
+  // data-aos="fade-up" data-aos-once="true"
   return (
     <div className={style.scrollOver}>
-      <h2 className={style.heading} data-aos="fade-up" data-aos-once="true">
-        IT'S GAME TIME
-      </h2>
+      <h2 className={style.heading}>IT'S GAME TIME</h2>
     </div>
   );
 }
