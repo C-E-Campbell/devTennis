83dec5c1f18a8446d6f4eee0754fd951a447d928
Charles <charles.e.campbell1989@gmail.com>
2020-07-06 13:13:30 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -1,20 +1,32 @@
-require("dotenv").config();
+require('dotenv').config({ path: '../.env' });
 const { PORT, SESSION_STRING, CONNECTION_STRING } = process.env;
-const express = require("express");
-const massive = require("massive");
-const session = require("express-session");
-const test = require("./controllers/testCtrl");
-const inventory = require("./controllers/inventoryCtrl");
-const auth = require("./controllers/authCtrl");
-const user = require("./controllers/userCtrl");
-const util = require("./controllers/utilCtrl");
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const test = require('./controllers/testCtrl');
+const inventory = require('./controllers/inventoryCtrl');
+const auth = require('./controllers/authCtrl');
+const user = require('./controllers/userCtrl');
+const util = require('./controllers/utilCtrl');
 
 const app = express();
 
-massive(CONNECTION_STRING).then(db => {
-  app.set("db", db);
-  console.log("db connected");
-});
+const db = massive(
+  {
+    connectionString: CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  {
+    scripts: '../db',
+  }
+)
+  .then((db) => {
+    app.set('db', db);
+    console.log('db connected');
+  })
+  .catch((err) => console.log(err));
 app.use(express.static(`${__dirname}/../build`));
 app.use(express.json());
 
@@ -24,37 +36,37 @@ app.use(
     resave: true,
     saveUninitialized: false,
     cookie: {
-      maxAge: 13452345235623623456326435636
-    }
+      maxAge: 13452345235623623456326435636,
+    },
   })
 );
 
-app.get("/api/test", test.test);
-app.get("/api/inventory", inventory.getAllInventory);
-app.get("/api/getcart/:id", inventory.getCart);
-app.get("/api/getfavorites/:id", inventory.favorites);
+app.get('/api/test', test.test);
+app.get('/api/inventory', inventory.getAllInventory);
+app.get('/api/getcart/:id', inventory.getCart);
+app.get('/api/getfavorites/:id', inventory.favorites);
 
-app.post("/api/register", auth.register);
-app.post("/api/login", auth.login);
-app.post("/api/addtocart", inventory.addToCart);
-app.post("/api/charge", util.sendPayment);
-app.post("/api/discount", util.sendDiscount);
-app.post("/api/receipt", util.sendReceipt);
-app.post("/api/addfavorite", inventory.addfavorites);
+app.post('/api/register', auth.register);
+app.post('/api/login', auth.login);
+app.post('/api/addtocart', inventory.addToCart);
+app.post('/api/charge', util.sendPayment);
+app.post('/api/discount', util.sendDiscount);
+app.post('/api/receipt', util.sendReceipt);
+app.post('/api/addfavorite', inventory.addfavorites);
 //app.post("/api/addimage", fileUpload.single("image"), auth.image);
 
-app.put("/api/updateEmail", user.updateEmail);
-app.put("/api/updatePass", user.updatePass);
-app.put("/api/decreasecart", inventory.decreaseCart);
-app.put("/api/deletefavorite", inventory.deleteFavorite);
+app.put('/api/updateEmail', user.updateEmail);
+app.put('/api/updatePass', user.updatePass);
+app.put('/api/decreasecart', inventory.decreaseCart);
+app.put('/api/deletefavorite', inventory.deleteFavorite);
 
-app.delete("/api/logout", auth.logout);
-app.delete("/api/deletecartitem/:id/:user", inventory.deleteItem);
-app.delete("/api/deletemyitems/:user_id", inventory.emptyAfterPurchase);
+app.delete('/api/logout', auth.logout);
+app.delete('/api/deletecartitem/:id/:user', inventory.deleteItem);
+app.delete('/api/deletemyitems/:user_id', inventory.emptyAfterPurchase);
 
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
